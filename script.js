df3051c8184e0437e2b0173f336afcad1039d0e4
gdeconcienciacorporal <156134961+gdeconcienciacorporal@users.noreply.github.com>
2024-02-19 10:15:54 -0300

    Semana 5 y 6

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-  actualizarDiasRestantes();
-  setInterval(actualizarDiasRestantes, 24*60*60*1000); // Actualizar diario
+  // actualizarDiasRestantes();
+  // setInterval(actualizarDiasRestantes, 24*60*60*1000); // Actualizar diario
    // Datos para el gráfico de líneas
 const dataLine1 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana-3',
-    data: [19, 8, 21, 15], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana-5',
+    data: [11, 11, 9, 18], // Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
     fill: false,
     tension: 0.1
@@ -16,8 +16,8 @@ const dataLine1 = {
 const dataLine2 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana-4',
-    data: [16, 9, 12, 0], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana-6',
+    data: [12, 12, 9, 20], // Datos de los km recorridos en cada día
     borderColor: 'rgba(153, 102, 255, 1)',
     fill: false,
     tension: 0.1
