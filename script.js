379f30c08e30a34b9321d015ef36d45a821e873b
gdeconcienciacorporal <156134961+gdeconcienciacorporal@users.noreply.github.com>
2024-03-18 09:15:27 -0300

    Modificación semana 8

--- script.js
+++ script.js
@@ -8,18 +8,18 @@ const dataLine1 = {
     label: 'Km recorridos en Semana 7',
     data: [18, 13, 11], // Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
-    fill: false,
+    fill: true,
     tension: 0.1
   }]
 };
 
 const dataLine2 = {
-  labels: ['Día 1', 'Día 2', 'Día 3'], // Etiquetas de los días
+  labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 8',
-    data: [14, 15, 9], // Datos de los km recorridos en cada día
+    label: 'Total Km recorridos en Semana 8: 38 km',
+    data: [14, 15, 9, 20], // Datos de los km recorridos en cada día
     borderColor: 'rgba(153, 102, 255, 1)',
-    fill: false,
+    fill: true,
     tension: 0.1
   }]
 };
